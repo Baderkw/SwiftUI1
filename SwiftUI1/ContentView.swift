c8a50130adef8eb654521c7b451841343371a08d
Baderkw <66323674+Baderkw@users.noreply.github.com>
2020-06-13 23:38:14 +0300

    Initial Commit

---
+++ SwiftUI1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI1
+//
+//  Created by Bader Alawadh on 6/13/20.
+//  Copyright © 2020 Bader Alawadh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
